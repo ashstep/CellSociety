25eb78b0f256dcca981b0482ce9d756b59d52cf3
Ashka <aas74@duke.edu>
2017-02-01 21:10:25 -0500

    added constructor for simulation

--- src/back_end/Fire/FireSim.java
+++ src/back_end/Fire/FireSim.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import back_end.Cell;
 import back_end.Simulation;
 import back_end.SimulationInfo;
+import back_end.gameOfLifePack.GameOfLifeCell;
 /**
  * Class that implements the unique properties of the fire simulation
  * @author Ashka Stephen
@@ -17,7 +18,24 @@ public class FireSim extends Simulation {
 	private final int[] COL_OFFSET = {0, 0,-1, 1};
 	private FireSimInfo myInfo;
 	
-	
+	/**
+	 * Constructor
+	 */
+	public FireSim(int[][] typeGrid){
+		int numRows = typeGrid.length;
+		int numCols = typeGrid[0].length;
+		FireCell[][] cellGrid = new FireCell[numRows][numCols];
+		for(int row = 0; row<numRows; row++){
+			for(int col = 0; col<numCols; col++){
+				cellGrid[row][col] = new FireCell(typeGrid[row][col]);
+			}
+		}
+		super.setGrid(cellGrid);
+	}
+
+	/**
+	 * Update the grid by creating copies of existing cells
+	 */
 	@Override
 	public Cell[][] updateGrid() {
 		Cell[][] newGrid=new Cell[getGrid().length][getGrid()[0].length];
@@ -32,6 +50,9 @@ public class FireSim extends Simulation {
 		return newGrid;
 	}
 	
+	/**
+	 * Getting the neighbors 
+	 */
 	@Override
 	protected ArrayList<Cell> getNeighbors(int row, int col) {
 		ArrayList<Cell> output=new ArrayList<Cell>();	
@@ -44,7 +65,6 @@ public class FireSim extends Simulation {
 		return output;
 	}
 	
-
 	@Override
 	protected int[] move(Cell[][] newGrid) {
 		// TODO Auto-generated method stub
@@ -54,7 +74,5 @@ public class FireSim extends Simulation {
 	public void setSimInfo(SimulationInfo newInfo) {
 		// TODO Auto-generated method stub
 	}
-
-
 
 }
