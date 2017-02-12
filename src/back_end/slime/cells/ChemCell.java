c51942359ffb7756420b62a21246fae8be4661ab
Ashka <aas74@duke.edu>
2017-02-12 12:15:51 -0500

    sim

--- src/back_end/slime/cells/ChemCell.java
+++ src/back_end/slime/cells/ChemCell.java
@@ -42,10 +42,6 @@ public class ChemCell extends SlimeCell{
 		this.campPercentage= another.campPercentage;
 	}
 
-
-	
-	
-	
 	@Override
 	public ActionBySim checkAndTakeAction(Collection<Cell> neighbors, SimulationInfo simInfo) {
 		return null;
