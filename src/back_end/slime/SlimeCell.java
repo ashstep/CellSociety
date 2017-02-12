34d6cf72b59cda76695c3cea029e6048a0d7f92f
Ashka <aas74@duke.edu>
2017-02-12 11:48:25 -0500

    get/set

--- src/back_end/slime/SlimeCell.java
+++ src/back_end/slime/SlimeCell.java
@@ -5,6 +5,11 @@ import java.util.Collection;
 import back_end.ActionBySim;
 import back_end.Cell;
 import back_end.SimulationInfo;
+import back_end.PredatorPrey.PPCells.EmptyPPCell;
+import back_end.PredatorPrey.PPCells.FishCell;
+import back_end.PredatorPrey.PPCells.SharkCell;
+import back_end.slime.cells.AgentCell;
+import back_end.slime.cells.ChemCell;
 import javafx.scene.paint.Color;
 
 public abstract class SlimeCell extends Cell {
@@ -76,13 +81,17 @@ public abstract class SlimeCell extends Cell {
 		return getMyType() == TYPE_CHEMICAL;
 	}
 	
+	
+	private boolean isAlive(){
+		return getMyType() == TYPE_ALIVE;
+	}
+
 	/**
 	 * getter for the time elapsed since chemical released
 	 */
 	public int getChemicalDiffusionTime(){
 		return timeElapsed;
 	}
-	
 	
 	/**
 	 * resetting the time elapsed since chemical released
@@ -98,45 +107,78 @@ public abstract class SlimeCell extends Cell {
 		timeElapsed++;
 	}
 	
-	
 	/**
 	 * getter for the chemical percent
 	 */
-	public double getChemicalPercentage(){
+	public double getChemicalPercent(){
 		return campPercentage;
 	}
-	
 	
 	/**
 	 * set the chemical percent
 	 */
-	public void setChemicalPercentage(double given){
+	public void setChemicalPercent(double given){
 		campPercentage = given;
 	}
 
+	/**
+	 * getter for the chemical percent
+	 */
+	public int getWiggleAngle(){
+		return wiggleAngle;
+	}
 	
-	
-	
-	
-	
+	/**
+	 * set the chemical percent
+	 */
+	public void setWiggleAngle(int given){
+		wiggleAngle = given;
+	}
 
-	campPercentage = 0;
-	wiggleAngle= 0;
-	probWiggle= 0;
-	sniffThreshold= 0;
-	sniffAngle= 0;
+	/**
+	 * getter 
+	 */
+	public int getWiggleProb(){
+		return probWiggle;
+	}
+	
+	/**
+	 * setter
+	 */
+	public void setWiggleProb(int given){
+		probWiggle = given;
+	}
 
+	/**
+	 * getter
+	 */
+	public int getSniffThresh(){
+		return sniffThreshold;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * setter
+	 */
+	public void setSniffThresh(int given){
+		sniffThreshold = given;
+	}
+
+	/**
+	 * getter
+	 */
+	public int getSniffAngle(){
+		return sniffAngle;
+	}
+
+	/**
+	 * setter
+	 */
+	public void setSniffAngle(int given){
+		sniffAngle = given;
+	}
+
+
+
 	
 
 	
@@ -146,7 +188,7 @@ public abstract class SlimeCell extends Cell {
 			return TYPE_ALIVE_COLOR;
 		} 
 		if(isChemical()){
-			return TYPE_CHEM_COLOR;
+			return TYPE_CHEMICAL_COLOR;
 		}
 		else {
 			return TYPE_EMPTY_COLOR;
@@ -167,9 +209,13 @@ public abstract class SlimeCell extends Cell {
 
 	@Override
 	public Cell makeCellofType(int type) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		if(type==TYPE_ALIVE){
+			return new AgentCell();
+		} else if(type==TYPE_CHEMICAL){
+			return new ChemCell();
+		} else {
+			throw new IllegalArgumentException("Invalid Cell Type");
+		}	}
 
 	@Override
 	public Cell makeNextStateCell() {
